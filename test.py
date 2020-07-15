0377c24064d779b2988f09d67355171238375c30
Manuel <manuelheller@outlook.com>
2020-07-15 14:29:31 +0200

    define maxvalue and minvalue

--- test.py
+++ test.py
@@ -1,53 +1,40 @@
-#from tictactoe import player,result
+from tictactoe import terminal, actions, result, utility, player
 
 X = "X"
 O = "O"
 EMPTY = None
 
-state = [[O, EMPTY, X],
-         [EMPTY, O, X],
-         [O, X, X]]
+board = [[EMPTY, X, O],
+         [O, X, EMPTY],
+         [X, EMPTY, O]]
 
-horizontal_X = [0,0,0]
-horizontal_O = [0,0,0]
-vertical_X = [0,0,0]
-vertical_O = [0,0,0]
+def maxvalue(board):
+    """
+    MAXVALUE picks action in ACTIONS(board) that produces highest value of MINVALUE(RESULT(board, action))
+    """
 
-for i in range(3):
-    for j in range(3):
-        if state[i][j] == X:
-            horizontal_X[i] += 1
-            vertical_X[j] += 1
-        if state[i][j] == O:
-            horizontal_O[i] += 1
-            vertical_O[j] += 1
+    if terminal(board) == True:
+        return(utility(board))
+    
+    v = float("-inf")
+    for a in actions(board):
+        v = max(v, minvalue(result(board, a)))
+    return(v)
 
-diagonal_X = [0,0]
-diagonal_O = [0,0]
+def minvalue(board):
+    """
+    MINVALUE picks action in ACTIONS(board) that produces lowest value of MAXVALUE(RESULT(board, action))
+    """
+   
+    if terminal(board) == True:
+        return(utility(board))
+    
+    v = float("inf")
+    for a in actions(board):
+        v = min(v, maxvalue(result(board, a)))
+    return(v)
 
-if state[0][0] == X and state[1][1] == X and state[2][2] == X:
-    diagonal_X[0] == 3
-if state[2][0] == X and state[1][1] == X and state[0][2] == X:
-    diagonal_X[1] == 3
-
-if state[0][0] == O and state[1][1] == O and state[2][2] == O:
-    diagonal_O[0] += 3
-if state[2][0] == O and state[1][1] == O and state[0][2] == O:
-    diagonal_O[1] += 3
-
-all_X = horizontal_X + vertical_X + diagonal_X
-all_O = horizontal_O + vertical_O + diagonal_O
-
-if max(all_X) == 3:
-    print(X)
-
-if max(all_O) == 3:
-    print(O)
-
-#for each x in horizontal vertical diagonal:
-#    if max(x) = 3
-
-#X_entries = [horizontal_X, vertical_X, diagonal_X]
-#print(X_entries)
-#max_Xvalues = list(min(s) for s in zip(*X_entries))
-#print(max_Xvalues)
+print(terminal(board))
+print(player(board))
+print(maxvalue(board))
+print(minvalue(board))
--- tictactoe.py
+++ tictactoe.py
@@ -164,3 +164,29 @@ def minimax(board):
     Returns the optimal action for the current player on the board.
     """
     raise NotImplementedError
+
+def maxvalue(board):
+    """
+    MAXVALUE picks action in ACTIONS(board) that produces highest value of MINVALUE(RESULT(board, action))
+    """
+
+    if terminal(board) == True:
+        return(utility(board))
+    
+    v = float("-inf")
+    for a in actions(board):
+        v = max(v, minvalue(result(board, a)))
+    return(v)
+
+def minvalue(board):
+    """
+    MINVALUE picks action in ACTIONS(board) that produces lowest value of MAXVALUE(RESULT(board, action))
+    """
+   
+    if terminal(board) == True:
+        return(utility(board))
+    
+    v = float("inf")
+    for a in actions(board):
+        v = min(v, maxvalue(result(board, a)))
+    return(v)
